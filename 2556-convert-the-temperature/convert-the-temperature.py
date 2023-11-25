8e6e6e10c9ac83791deb9ed1cbfe61a7e0a743da
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2023-11-25 10:57:21 +0530

    Time: 35 ms (72.79%) | Memory: 16.4 MB (30.51%) - LeetSync

--- 2556-convert-the-temperature/convert-the-temperature.py
+++ 2556-convert-the-temperature/convert-the-temperature.py
@@ -1,7 +1,4 @@
 class Solution:
     def convertTemperature(self, celsius: float) -> List[float]:
-        kelvin = celsius + 273.15
-        fahren = celsius * 1.80 + 32.00
-
-        return [kelvin, fahren]
+        return [celsius + 273.15, celsius * 1.80 + 32.00]
         
