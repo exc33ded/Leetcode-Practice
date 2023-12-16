750557a99825f2d481c9ab7d2f46690eb1ba2701
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2023-12-16 12:37:58 +0530

    Time: 29 ms (96.69%) | Memory: 16.2 MB (80.31%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.py
+++ 782-jewels-and-stones/jewels-and-stones.py
@@ -1,12 +1,21 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
         # Brute Force
+
+        # counter = 0
+        # for j in jewels:
+        #     for s in stones:
+        #         if j == s:
+        #             counter += 1        
+        # return counter
+        
+
+        # Optimized
+        set_jewels = set(jewels)
         counter = 0
 
-        for j in jewels:
-            for s in stones:
-                if j == s:
-                    counter += 1
+        for item in stones:
+            if item in set_jewels:
+                counter += 1
         
         return counter
-        
