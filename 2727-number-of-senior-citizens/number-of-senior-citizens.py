72346d12e6c1ef192561ec66994ca685e658b6e0
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2024-02-06 15:29:14 +0530

    Time: 37 ms (98.26%) | Memory: 16.5 MB (79.58%) - LeetSync

---
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        counter = 0
+        for age in details:
+            if int(age[11:13]) > 60:
+                counter += 1
+
+        return counter
