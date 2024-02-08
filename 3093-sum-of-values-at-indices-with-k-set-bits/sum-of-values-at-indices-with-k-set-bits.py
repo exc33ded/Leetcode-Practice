5d736e46d0aff11b85aebefe71de3a08749340c5
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2024-02-08 11:57:29 +0530

    Time: 86 ms (27.32%) | Memory: 16.8 MB (58.53%) - LeetSync

---
+++ 3093-sum-of-values-at-indices-with-k-set-bits/sum-of-values-at-indices-with-k-set-bits.py
@@ -0,0 +1,20 @@
+class Solution:
+    def sumIndicesWithKSetBits(self, nums: List[int], k: int) -> int:
+        ## Brute 
+
+        def to_binary_one(num):
+            count_one = 0
+            while (num > 0):
+                rem = num % 2
+                if rem == 1:
+                    count_one += 1
+                num = num // 2
+            return count_one
+
+        sum_ = 0
+        for i in range(len(nums)):
+            if to_binary_one(i) == k:
+                sum_ += nums[i]
+
+        return sum_
+                
