cbe1916869b65accdbf1ea88dfd12b11253279f1
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2024-03-19 13:12:38 +0530

    Time: 55 ms (30.09%) | Memory: 16.9 MB (58.19%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
@@ -1,16 +1,19 @@
 class Solution:
     def applyOperations(self, nums: List[int]) -> List[int]:
-        for i in range(0,len(nums)-1):
-            if nums[i]==nums[i+1]:
-                nums[i]*=2
-                nums[i+1]=0
-        
-        lst = [0] * len(nums)
-        
+        i = 0
+        while i < len(nums) - 1:
+            if nums[i] == nums[i + 1]:
+                nums[i] *= 2
+                nums[i + 1] = 0
+            i += 1
+
+        # Rearrange non-zero elements to the left
         j = 0
         for i in range(len(nums)):
             if nums[i] != 0:
-                lst[j] = nums[i]
+                nums[j] = nums[i]
+                if j != i:
+                    nums[i] = 0
                 j += 1
-        
-        return lst
+
+        return nums
