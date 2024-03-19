cbafe7836114612168434f5368b0808cd5b0e28b
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2024-03-19 13:03:03 +0530

    Time: 1 ms (93.28%) | Memory: 43.5 MB (75.60%) - LeetSync

---
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        // Combine consecutive equal elements
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
+            }
+        }
+
+        // Rearrange non-zero elements to the left
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[j] = nums[i];
+                if (j != i) {
+                    nums[i] = 0;
+                }
+                j++;
+            }
+        }
+
+        return nums;
+    }
+}
