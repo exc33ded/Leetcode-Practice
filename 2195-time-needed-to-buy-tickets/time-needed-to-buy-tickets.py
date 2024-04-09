4db510215a924c8f6816b1a832a0b726f2844639
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2024-04-09 10:38:44 +0530

    Time: 40 ms (80.36%) | Memory: 16.6 MB (51.83%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
@@ -1,17 +1,7 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        time, i = 0, 0
-        while tickets[k] != 0 and i < len(tickets):
-            if tickets[i] != 0:
-                tickets[i] -= 1
-                time += 1
-                
-            i += 1  # Move to the next person
-
-            if i == len(tickets):
-                i = 0  # Reset to the first person if reached the end of the list
-            
-            if i == k and tickets[i] == 0:
-                return time
-
-        return time
+        res = 0
+        for i in range(len(tickets)):
+            if i <= k: res+=min(tickets[i], tickets[k])
+            else: res+=min(tickets[i], tickets[k]-1)
+        return res
