e7e17c8bdb3744ac11fd936ca2f42e01e8f5fe77
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2024-05-23 18:31:54 +0530

    Time: 41 ms (28.93%) | Memory: 16.5 MB (89.72%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,21 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        zero_cnt, one_cnt, two_cnt = 0,0,0
-        for num in nums:
-            if num == 0:
-                zero_cnt += 1
-            elif num == 1:
-                one_cnt += 1
-            else:
-                two_cnt += 1
+        zero = nums.count(0)
+        one = nums.count(1)
+        two = nums.count(2)
 
-        z_arr = [0]*zero_cnt
-        o_arr = [1]*one_cnt
-        t_arr = [2]*two_cnt
-        arr = []
-        arr[:] = z_arr[:] + o_arr[:] + t_arr[:]
-        
         for i in range(len(nums)):
-            nums[i] = arr[i]
-        return nums
+            if zero > 0:
+                nums[i] = 0
+                zero -= 1
+            elif one > 0:
+                nums[i] = 1
+                one -= 1
+            else:
+                nums[i] = 2
+        
