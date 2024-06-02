d23da72bca68bdeb4da88af505f56f6d2b75877c
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2024-06-02 13:11:07 +0530

    Time: 77 ms (47.32%) | Memory: 17.8 MB (80.13%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,36 +1,33 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        
-        def lastOcc(nums, target):
+        def floor(nums, target):
             start = 0
-            last = len(nums) - 1
-            last_occ = -1
-            while(start <= last):
-                mid = start + (last - start)//2
-                if (nums[mid] > target):
-                    last = mid - 1
-                elif (nums[mid] < target):
+            end = len(nums) - 1
+            idx = -1
+            while(start <= end):
+                mid = start + (end - start)//2
+                if nums[mid] == target:
+                    idx = mid
                     start = mid + 1
+                elif nums[mid] > target:
+                    end = mid - 1
                 else:
-                    last_occ = mid
                     start = mid + 1
-            return last_occ
-
-        def firstOcc(nums, target):
+            return idx
+           
+        def ceil(nums, target):
             start = 0
-            last = len(nums) - 1
-            first_occ = -1
-            while(start <= last):
-                mid = start + (last - start)//2
-                if (nums[mid] > target):
-                    last = mid - 1
-                elif (nums[mid] < target):
-                    start = mid + 1
+            end = len(nums) - 1
+            idx = -1
+            while(start <= end):
+                mid = start + (end - start)//2
+                if nums[mid] == target:
+                    idx = mid
+                    end = mid - 1
+                elif nums[mid] > target:
+                    end = mid - 1
                 else:
-                    first_occ = mid
-                    last = mid - 1
-            return first_occ
+                    start = mid + 1
+            return idx
 
-        return [firstOcc(nums, target), lastOcc(nums, target)]
-
-        
+        return [ceil(nums, target), floor(nums, target)]
