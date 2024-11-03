d60cf2f14c9f08e450d348a15219f5d14099b3a2
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2024-11-03 11:10:33 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (40.35%) - LeetSync

--- 812-rotate-string/rotate-string.py
+++ 812-rotate-string/rotate-string.py
@@ -1,11 +1,5 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        """
-        When the s string is combined (s + s), we get a string which contains the pattern 
-        of the rotated inside the merged string so we can check then if the goal string 
-        exist there or not.
-        """
         if len(s) == len(goal):
-            if goal in s + s: 
-                return True
+            return goal in s + s
         return False
